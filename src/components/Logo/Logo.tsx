07387f5f44c564443d2b65edc8aebbeb51e195d3
bradleygichuru <bradleygichuru@gmail.com>
2025-03-08 16:47:38 +0300

    feat:changes logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import React from 'react'
-
+import logo from "../../../public/logo.svg"
+import Image from 'next/image'
 interface Props {
   className?: string
   loading?: 'lazy' | 'eager'
@@ -15,15 +16,13 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
+    <Image
+      alt="Hizitickets Logo"
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      src={logo}
+      className='dark:stroke-white stroke-black'
     />
   )
 }
